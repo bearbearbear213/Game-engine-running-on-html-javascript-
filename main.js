8eedc076ac75b15f83c42cba433a9917c8419433
bearbearbear213 <youmotengyuan2@gmail.com>
2025-03-27 18:44:58 +0900

    Update main.js

--- main.js
+++ main.js
@@ -683,8 +683,8 @@
     return text.split("/")
   }
   class novelGame extends Game {
-    constructor(seen, fps, statrSeen) {
-      super(seen, fps, statrSeen,true);
+    constructor(seen, fps, statrSeen,useController=true) {
+      super(seen, fps, statrSeen,useController);
       this.speechColor = "rgb(0,0,0)";
       this.speechOpening = false;
       this.speechText = "";
